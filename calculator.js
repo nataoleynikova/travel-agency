ff227636ea685c3dc428aea4c8ba380c2ce41629
Nata <olejnikova-n@yandex.ru>
2021-09-24 19:49:56 +0300

    исправлен jquery

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  // Связка days и nights
+  // Связка дня и ночи
   $('#days').bind('input', function() {
     let value = parseInt(event.target.value);
     if (Number.isFinite(value)) {
@@ -15,15 +15,14 @@ $(document).ready(function(){
   });
 
   // Калькулятор
-  $(function calc() {
-    $('#country') = country.options[country.selectedIndex].value;
-    $('#days').val() = Number(days);
-    $('#nights').val() = Number(nights) / 2;
-    $('#people').val() = Number(people);
-    let result = $('#select').val() * ($('#days').val() + 
-    $('#nights').val()) * $('#people').val();
-    console.log (result);
-    $('#result').html('result');
-    console.log (4);
+  $('button').on('click', function() {
+    // let country = Number($('#country').val());
+    // let days = Number($('#days').val());
+    // let nights = Number($('#nights').val()) / 2;
+    // let people = Number($('#people').val());
+    // let result = country * (days + nights) * people;
+    let result = Number($('#country').val()) * (Number($('#days').val()) + 
+    Number($('#nights').val()) / 2) * Number($('#people').val());
+    $('#result').html(result);
   });
 });
